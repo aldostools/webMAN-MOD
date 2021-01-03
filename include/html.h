b2451fe41e98075cb18b0c05c8a032ecb4914a85
kotborealis <kotborealis@gmail.com>
2021-01-03 15:30:29 +0300

    Disabled CORS
    Cross-Origin Resource Sharing could lead to security problems:
    malicious website could scan local network for PS3 w/ Webman-mod webserver
    and execute arbitrary code on it. 
    Proof of concept: https://github.com/kotborealis/webman-mod-cors-poc

--- include/html.h
+++ include/html.h
@@ -417,7 +417,6 @@ static size_t prepare_header(char *buffer, const char *param, u8 is_binary)
 	bool set_base_path = false;
 
 	size_t slen = sprintf(buffer,	"HTTP/1.1 200 OK\r\n"
-									"Access-Control-Allow-Origin: *\r\n"
 									"Content-Type: "); char *header = buffer + slen;
 
 	int flen = strlen(param);
